5f1fa21aca5bcf4e61274e23e52c280c136a4026
School <kaboy@icloud.com>
2022-03-09 19:33:58 -0600

    index.js initial setup of inquirer and fs

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = requires('fs');
+const myTeam = require('./src/myTeam');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
