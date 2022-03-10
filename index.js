4c4cc8683e8a476c201d0256488e4a9888231102
School <kaboy@icloud.com>
2022-03-09 20:27:20 -0600

    added manager's name prompt

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const myTeam = require('./src/myTeam');
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const myTeam = require('./src/myTeam');
+
+const workArray = [];
+
+const addManager = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Welcome, Team Manager! Please enter your name.',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name.");
+          return false;
+        }
+      }
+    },
