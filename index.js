29a6b42dc5e340a7fbbf3874b1be146f93e83f40
School <kaboy@icloud.com>
2022-03-09 20:39:05 -0600

    add employee ID to manager input

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -7,8 +8,10 @@ const Intern = require('./lib/Intern');
 
 const myTeam = require('./src/myTeam');
 
+// Create an array of questions for user input
 const workArray = [];
 
+// Prompt to enter the team manager’s name, employee ID, email address, and office number
 const addManager = () => {
   return inquirer.prompt([
     {
@@ -21,6 +24,19 @@ const addManager = () => {
         } else {
           console.log("Please enter your name.");
           return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Please enter your employee ID.',
+      validate: idInput => {
+        if (isNaN(idInput)) { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+          console.log("Try again. Please enter your employee ID.");
+          return false;
+        } else {
+          return true;
         }
       }
     },
