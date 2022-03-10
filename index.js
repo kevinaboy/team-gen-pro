5e0be642ecdf58ef9bcb812c03d3864eb41c4778
School <kaboy@icloud.com>
2022-03-09 20:00:21 -0600

    fixed syntax errors in Intern.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
-const fs = requires('fs');
+const fs = require('fs');
 const myTeam = require('./src/myTeam');
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
